52e9c37a258e50916dbaa5f40fc49bb40647672e
Michael Gipson <mgipsy101@gmail.com>
2020-10-07 18:48:44 -0400

    Added 2 new rooms, plus a "game over" prompt.

--- haunted_house.py
+++ haunted_house.py
@@ -3,6 +3,10 @@ import random
 USER_LIFE_POINTS = 20
 
 
+def game_over():
+    print("You have died. Better luck in the afterlife!")
+
+
 def fight_ghosts():
     global USER_LIFE_POINTS
     print("The ghosts are attacking us!!!!")
@@ -11,6 +15,9 @@ def fight_ghosts():
         print("Ghost number ", i, " attacked us!")
         USER_LIFE_POINTS -= 1
     print("Life points have been reduced to ", USER_LIFE_POINTS)
+    if USER_LIFE_POINTS <= 0:
+        game_over()
+
     response = input("We see a chest in th corner! Should we open it (yes or no)?")
     if response == "yes":
         pass
@@ -18,31 +25,68 @@ def fight_ghosts():
         pass
 
 
-def enter_left_room():
+def enter_room1_left():
     response = input("You find some ghosts who are attacking you! What do you do: run away or fight?")
     if response == "fight":
         fight_ghosts()
     elif response == "run away":
+        print("You run back the way you came, finding yourself back at the beginning.")
         enter_the_house()
 
 
-def enter_right_room():
+def hall1_death():
+    global USER_LIFE_POINTS
+    print("As you find yourself lost in the darkness, you begin to notice heavy breathing behind you. /n"
+          "Your heart rate slows to a crawl as you find yourself unable to turn around. You lose consciousness as long, thin fingers wrap themselves around your neck. /n"
+          "You find yourself within a dream that you will never wake up from.")
+    USER_LIFE_POINTS -= 500
+
+    if USER_LIFE_POINTS <= 0:
+        game_over()
+
+
+def enter_room2_right():
     pass
 
 
+def enter_room2():
+    print("You enter a long hallway, lights flickering dimly throughout the long hall. "
+          "/n You hear a slow, methodical scraping noise from the darkness at the end of the hall.")
+    response = input("You see a door on your right. Will you open it, or will you proceed down the hallway?")
+    while True:
+        if response == "proceed" or response == "right":
+            break
+        else:
+            print("Time passes as you ponder your decision.")
+            response = input("You see a door on your right. Will you open it, or will you proceed down the hallway?")
+            break
+
+    if response == "proceed":
+        print("You press deeper into the darkness, the scratching becoming clearer and clearer the farther you go.")
+        hall1_death()
+    if response == "right":
+        enter_room2_right()
+
+
+def enter_room1_right():
+    enter_room2()
+
+
 def enter_the_house():
-    print("Welcome to the haunted house!")
-    response = input("You see two doors, one to the left, and one to your right! Which one do you want to open?")
+    print("You approach a Haunted House. The night creeps up upon you as you enter.")
+    response = input("Once inside, you see two doors. One to the left, and one to your right. Which one will you open?")
     while True:
         if response == "left" or response == "right":
             break
         else:
-            print("Enter a valid room!")
-    response = input("You see two doors, one to the left, and one to your right! Which one do you want to open?")
+            print("Time passes as you ponder your decision.")
+            response = input(
+                "Two doors remain on your left and your right as rain begins to fall outside. Which will you choose?")
+            break
     if response == "left":
-        enter_left_room()
+        enter_room1_left()
     if response == "right":
-        enter_right_room()
+        enter_room1_right()
 
 
 def run():
