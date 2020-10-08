666fbdb72b80f8340bfe034ddecf7c8e733292f1
Michael Gipson <mgipsy101@gmail.com>
2020-10-08 13:04:47 -0400

    Finished floor 2 and added responses for chest opening after fight. Also added health potion variable to recover life points.

--- haunted_house.py
+++ haunted_house.py
@@ -1,15 +1,16 @@
 import random
 
 USER_LIFE_POINTS = 20
+health_potion = random.randint(1, 5)
 
 
 def game_over():
     print("You have died. Better luck in the afterlife!")
 
 
-def fight_ghosts():
+def floor1_fight():
     global USER_LIFE_POINTS
-    print("The ghosts are attacking us!!!!")
+    print("You are attacked by a crowd of Ghosts.")
     number_of_ghosts = random.randint(1, 5)
     for i in range(number_of_ghosts):
         print("Ghost number ", i, " attacked us!")
@@ -18,26 +19,57 @@ def fight_ghosts():
     if USER_LIFE_POINTS <= 0:
         game_over()
 
-    response = input("We see a chest in th corner! Should we open it (yes or no)?")
+    response = input("We see a chest in the corner! Should we open it (Type yes or no)?")
     if response == "yes":
-        pass
+        print("You find a bottle of mysterious red liquid. Drinking it recovers some of your life points.")
+        USER_LIFE_POINTS += health_potion
+        print("You recover ", health_potion, " life points.")
+        print("Your life point total is now ", USER_LIFE_POINTS, ".")
     elif response == "no":
-        pass
+        print("You ignore the shiny chest in front of you and proceed to the next floor.")
+        enter_floor2()
 
 
-def enter_room1_left():
-    response = input("You find some ghosts who are attacking you! What do you do: run away or fight?")
+def enter_floor3():
+    pass
+
+
+def floor2_fight():
+    global USER_LIFE_POINTS
+    print("You are attacked by a crowd of Ghosts.")
+    number_of_ghosts = random.randint(1, 5)
+    for i in range(number_of_ghosts):
+        print("Ghost number ", i, " attacked us!")
+        USER_LIFE_POINTS -= 1
+    print("Life points have been reduced to ", USER_LIFE_POINTS)
+    if USER_LIFE_POINTS <= 0:
+        game_over()
+
+    response = input("We see a chest in the corner! Should we open it (Type yes or no)?")
+    if response == "yes":
+        print("You find a bottle of mysterious red liquid. Drinking it recovers some of your life points.")
+        USER_LIFE_POINTS += health_potion
+        print("You recover ", health_potion, " life points.")
+        print("Your life point total is now ", USER_LIFE_POINTS, ".")
+    elif response == "no":
+        print("You ignore the shiny chest in front of you and proceed to the next floor.")
+        enter_floor3()
+
+
+def enter_floor1_left():
+    response = input(
+        "You find some ghosts who are attacking you! What do you do: run away or fight? (Type fight or run away)")
     if response == "fight":
-        fight_ghosts()
+        floor1_fight()
     elif response == "run away":
         print("You run back the way you came, finding yourself back at the beginning.")
         enter_the_house()
 
 
-def hall1_death():
+def floor2_death():
     global USER_LIFE_POINTS
-    print("As you find yourself lost in the darkness, you begin to notice heavy breathing behind you. /n"
-          "Your heart rate slows to a crawl as you find yourself unable to turn around. You lose consciousness as long, thin fingers wrap themselves around your neck. /n"
+    print("As you find yourself lost in the darkness, you begin to notice heavy breathing behind you. \n"
+          "Your heart rate slows to a crawl as you find yourself unable to turn around. You lose consciousness as long, thin fingers wrap themselves around your neck. \n"
           "You find yourself within a dream that you will never wake up from.")
     USER_LIFE_POINTS -= 500
 
@@ -45,14 +77,21 @@ def hall1_death():
         game_over()
 
 
-def enter_room2_right():
-    pass
+def enter_floor2_right():
+    response = input(
+        "You encounter a group of Ghosts huddled around a small table. Will you Fight or Run Away? (Type fight or run away)")
+    if response == "fight":
+        floor2_fight()
+    elif response == "run away":
+        print("You run back the way you came, finding yourself back to where you were before.")
+        enter_floor2()
 
 
-def enter_room2():
+def enter_floor2():
     print("You enter a long hallway, lights flickering dimly throughout the long hall. "
-          "/n You hear a slow, methodical scraping noise from the darkness at the end of the hall.")
-    response = input("You see a door on your right. Will you open it, or will you proceed down the hallway?")
+          "\n You hear a slow, methodical scraping noise from the darkness at the end of the hall.")
+    response = input(
+        "You see a door on your right. Will you open it, or will you proceed down the hallway? (Type proceed or right)")
     while True:
         if response == "proceed" or response == "right":
             break
@@ -63,30 +102,31 @@ def enter_room2():
 
     if response == "proceed":
         print("You press deeper into the darkness, the scratching becoming clearer and clearer the farther you go.")
-        hall1_death()
+        floor2_death()
     if response == "right":
-        enter_room2_right()
+        enter_floor2_right()
 
 
-def enter_room1_right():
-    enter_room2()
+def enter_floor1_right():
+    enter_floor2()
 
 
 def enter_the_house():
     print("You approach a Haunted House. The night creeps up upon you as you enter.")
-    response = input("Once inside, you see two doors. One to the left, and one to your right. Which one will you open?")
+    response = input(
+        "Once inside, you see two doors. One to the left, and one to your right. Which one will you open? (Type right or left)")
     while True:
         if response == "left" or response == "right":
             break
         else:
             print("Time passes as you ponder your decision.")
             response = input(
-                "Two doors remain on your left and your right as rain begins to fall outside. Which will you choose?")
+                "Two doors remain on your left and your right as rain begins to fall outside. Which will you choose? (Type right or left)")
             break
     if response == "left":
-        enter_room1_left()
+        enter_floor1_left()
     if response == "right":
-        enter_room1_right()
+        enter_floor1_right()
 
 
 def run():
