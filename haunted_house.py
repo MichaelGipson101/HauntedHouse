83343b4b6abb79faf5556dac77acde306c6333e1
Michael Gipson <mgipsy101@gmail.com>
2020-10-13 17:31:33 -0400

    Finished Floor 3, and Final Floor.

--- haunted_house.py
+++ haunted_house.py
@@ -1,6 +1,7 @@
 import random
 
 USER_LIFE_POINTS = 20
+MONSTER_LIFE_POINTS = 10
 health_potion = random.randint(1, 5)
 
 
@@ -26,7 +27,7 @@ def floor1_fight():
     if USER_LIFE_POINTS <= 0:
         game_over()
 
-    response = input("We see a chest in the corner! Should we open it (Type yes or no)?")
+    response = input("You see a chest in the corner. Will you open it? (Type yes or no)")
     if response == "yes":
         print("You find a bottle of mysterious red liquid. Drinking it recovers some of your life points.")
         USER_LIFE_POINTS += health_potion
@@ -42,12 +43,62 @@ def floor1_fight():
 
 
 def final_fight():
-    victory()
-    game_over()
+    global USER_LIFE_POINTS
+    global MONSTER_LIFE_POINTS
+    print("The monster in front of you attacks.")
+    monster = 1
+    for i in range(monster):
+        print("You are slashed by the Monster's claws, leaving a large red mark across your chest. It begins to bleed.")
+        USER_LIFE_POINTS -= 5
+    print("Life points have been reduced to ", USER_LIFE_POINTS)
+    if USER_LIFE_POINTS <= 0:
+        game_over()
+
+    response = input("Will you fight back? (Type yes or no)?")
+    if response == "yes":
+        print("You flail your arms at the monster, attempting to hit it.")
+        number_of_hits = random.randint(1, 5)
+        for i in range(number_of_hits):
+            print("You land a hit!")
+            MONSTER_LIFE_POINTS -= 5
+        print("The Monster's Life points have been reduced to ", MONSTER_LIFE_POINTS)
+        if MONSTER_LIFE_POINTS <= 0:
+            print(
+                "Though you bested the monster in front of you, you don't notice a box behind you as you move away from the now-dead Monster."
+                "\nTime slows to a halt as you lose your footing, tumbling through the glass window behind you.")
+            victory()
+    elif response == "no":
+        print(
+            "You remain frozen in place. The monster hesitates for only a moment before driving its claws into your chest.")
+        game_over()
 
 
 def enter_final_floor():
-    pass
+    print(
+        "You enter the attic of the house. Most of the space is covered by boxes and other items. \nYou notice that the"
+        "Monster is in the back of the room, but it has yet to notice you."
+        "\nYou also notice that there is an open window on the other side of the room.")
+    response = input(
+        "Will you try to escape, or confront the monster who has been haunting you? (Type escape or fight)")
+    while True:
+        if response == "escape" or response == "fight":
+            break
+        else:
+            print("Time passes as you ponder your decision.")
+            response = input(
+                "Will you try to escape, or confront the monster who has been haunting you? (Type escape or fight)")
+            break
+
+    if response == "escape":
+        print(
+            "You manage to find your way to the window without alerting the Monster. \nHowever, as you try to climb out,"
+            "you slip. You let out a scream as you fall.")
+        victory()
+    if response == "fight":
+        print(
+            "The Monster stirs as you approach, coming to its feet with alarming speed. For a moment, it stares at you."
+            "\nIt reaches out with its long, tendril-like claws to attack you.")
+        final_fight()
 
 
 def enter_floor9():
@@ -75,15 +126,78 @@ def enter_floor4():
 
 
 def floor_3_left():
-    pass
+    global USER_LIFE_POINTS
+    print(
+        "You struggle to fit through the small opening. It is damp, and you flinch as bugs and spiders skitter around you."
+        "\nYou eventually find yourself in a small room, with a large vest laying on the floor in front of you.")
+    response = input("We see a chest in the corner! Should we open it (Type yes or no)?")
+    if response == "yes":
+        print(
+            "You put the vest on. Though it weighs down on you, you feel as if you could take a few more hits than usual.")
+        USER_LIFE_POINTS += 10
+        print("You gain an additional 10 Life Points.")
+        print("Your life point total is now ", USER_LIFE_POINTS, ".")
+        print("You see a ladder leading up to the next floor.")
+        enter_floor4()
+    elif response == "no":
+        print(
+            "You ignore the vest, thinking that the extra weight would slow you down. Instead, you see a ladder leading up to the next floor.")
+        enter_floor4()
+
+
+def enter_bonus_floor():
+    global USER_LIFE_POINTS
+    print("You hit a concrete floor below you, twisting your ankle in the process.")
+    USER_LIFE_POINTS -= 7
+    print("Your life point total is now ", USER_LIFE_POINTS, ".")
+    print("After the pain subsides, you notice that there is a large spiral staircase in the corner of the room."
+          "\n There is also a door to your left leading somewhere else.")
+    response = input(
+        "Which staircase will you take? (Type forward or left)")
+    while True:
+        if response == "forward" or response == "left":
+            break
+        else:
+            print("Time passes as you ponder your decision.")
+            response = input(
+                "Which staircase will you take? (Type forward or left)")
+            break
+
+    if response == "forward":
+        print("The spiral staircase takes you much farther up into the house than you would have expected."
+              "\nYou find yourself on a new floor.")
+        enter_floor5()
+    if response == "left":
+        print("The staircase to your left takes you to an unfamiliar hallway above where you were before.")
+        enter_floor4()
 
 
 def floor_3_forward():
-    pass
+    print("You enter a large bedroom, presumably for whoever lived in the house before it was abandoned.\n"
+          "The room stinks of perfume, an odd scent for a place long since abandoned."
+          "\nAs you approach the bed at the center of the room, you step on a loose floorboard. You fall through the floor.")
+    enter_bonus_floor()
 
 
 def enter_floor3():
-    pass
+    print(
+        "You enter a large, dark room. There is nothing inside except for a lone wooden chair, and a spotlight above it."
+        "\nYou notice that there is a large wooden door in front of you, and a small crawl space to your left. Which will you choose?")
+    response = input(
+        "Will you take the door in front of you, or try your hand in the crawl space? (Type forward or left)")
+    while True:
+        if response == "forward" or response == "left":
+            break
+        else:
+            print("Time passes as you ponder your decision.")
+            response = input(
+                "Will you take the door in front of you, or try your hand in the crawl space? (Type forward or left)")
+            break
+
+    if response == "forward":
+        floor_3_forward()
+    if response == "left":
+        floor_3_left()
 
 
 def floor2_fight():
@@ -97,7 +211,7 @@ def floor2_fight():
     if USER_LIFE_POINTS <= 0:
         game_over()
 
-    response = input("We see a chest in the corner! Should we open it (Type yes or no)?")
+    response = input("You see a chest in the corner. Will you open it? (Type yes or no)")
     if response == "yes":
         print("You find a bottle of mysterious red liquid. Drinking it recovers some of your life points.")
         USER_LIFE_POINTS += health_potion
@@ -179,7 +293,8 @@ def enter_floor2():
             break
         else:
             print("Time passes as you ponder your decision.")
-            response = input("You see a door on your right. Will you open it, or will you proceed down the hallway?")
+            response = input(
+                "You see a door on your right. Will you open it, or will you proceed down the hallway? (Type proceed or right)")
             break
 
     if response == "proceed":
@@ -202,7 +317,8 @@ def enter_floor1_right():
 def enter_the_house():
     print("You approach a Haunted House. The night creeps up upon you as you enter.")
     response = input(
-        "Once inside, you see two doors. One to the left, and one to your right. Which one will you open? (Type right "
+        "Once inside, the door behind you locks. You will have to advance through the house to escape."
+        "\nIn front of you, there are two doors. One to the left, and one to your right. Which one will you open? (Type right "
         "or left)")
     while True:
         if response == "left" or response == "right":
